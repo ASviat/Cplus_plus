47da921003cd198a270c4c4a6ca5b8fd130daa12
Sviatoslav <ecrprague.asi@gmail.com>
2022-11-14 15:03:53 +0300

    Stih

---
+++ 14.11_2/Stih.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My heart’s in the Highlands, my heart is not here;" << endl
+         << "My heart’s in the Highlands, a-chasing the deer;" << endl
+         << "A-chasing the wild deer, and following the roe," << endl
+         << "My heart’s in the Highlands wherever I go." << endl;
+    return 0;
+}

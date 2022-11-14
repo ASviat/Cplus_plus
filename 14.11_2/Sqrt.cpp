308334656f1c12f71529b3a6180d91f99e61b90e
Sviatoslav <ecrprague.asi@gmail.com>
2022-11-14 14:10:12 +0300

    Sqrt Done

---
+++ 14.11_2/Sqrt.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	double number,answer;
+	cout << "Enter number: ";
+	cin >> number;
+	answer = sqrt(number);
+	cout << "Sqrt = " << answer << endl;
+	return 0;
+}
